c7005a5e75f986055b2f3f25722cab75d0d3d439
Michael Jung <mijung@gmx.net>
2015-10-29 21:34:43 +0100

    Updated licensing information

--- src/tests/tlv_test.c
+++ src/tests/tlv_test.c
@@ -1,3 +1,21 @@
+/*
+ * libtlv - Support Library for EMV TLV handling.
+ * Copyright (C) 2015 Michael Jung <mijung@gmx.net>, All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <check.h>
