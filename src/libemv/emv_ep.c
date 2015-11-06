b349d1bbe4abf54910e821b4ea83935059f831d5
Michael Jung <mijung@gmx.net>
2015-11-06 20:37:11 +0100

    Adding license headers

--- src/libemv/emv_ep.c
+++ src/libemv/emv_ep.c
@@ -1,5 +1,22 @@
-#include <string.h>
+/*
+ * libemv - Support Library containing EMVCo L2 Kernels.
+ * Copyright (C) 2015 Michael Jung <mijung@gmx.net>, All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
 
+#include <string.h>
 #include <emv.h>
 
 #define REQUIREMENT(book, id)
